75d5f9a437f59b92ed2a106be7b96809f28d31cf
Alvin Ababil <neoryumasil@gmail.com>
2026-05-30 19:44:36 +0700

    Fix Blog Bug when CRUD

--- src/app/api/blog/[id]/route.ts
+++ src/app/api/blog/[id]/route.ts
@@ -2,6 +2,29 @@ import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 import { cookies } from 'next/headers';
 
+
+// Get function for details
+export async function GET( request: Request, { params }: { params: Promise<{ id: string }> }) {
+    try {
+        const { id } = await params;
+        const id_blog = Number(id);
+
+        const blogDetail = await prisma.blog.findUnique({
+            where: { id_blog },
+            include: { admin: { select: { username: true } } } 
+        });
+
+        if (!blogDetail) {
+            return NextResponse.json({ message: "Artikel tidak ditemukan" }, { status: 404 });
+        }
+
+        return NextResponse.json({ message: "Berhasil mengambil detail artikel", data: blogDetail }, { status: 200 });
+    } catch (error) {
+        console.error("GET Detail Blog error:", error);
+        return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
+    }
+}
+
 // PUT function to update a blog post based on id
 export async function PUT(request: Request, { params }: { params: Promise<{ id: string }> }) {
     try {
