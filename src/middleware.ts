c920767ffb2cb6d6005053d4f061f8d9af943a61
Alvin Ababil <neoryumasil@gmail.com>
2026-06-02 11:27:39 +0700

    Now Hide Swagger PAth

--- src/middleware.ts
+++ src/middleware.ts
@@ -65,6 +65,7 @@ export async function middleware(request: NextRequest) {
 export const config = {
     matcher: [
         "/admin/:path*", 
+        "/api-docs/:path*",
         "/login"
     ],
 };
